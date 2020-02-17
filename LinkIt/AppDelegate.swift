0be5e2762bf077a1c7bfc399f926ace22a85417e
Luis Güette <guetteluis@gmail.com>
2020-02-16 23:48:40 -0400

    Initial Commit

---
+++ LinkIt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LinkIt
+//
+//  Created by Luis Guette on 2/16/20.
+//  Copyright © 2020 Luis Guette. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
