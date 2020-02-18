a345ce205e7f2b1a6ae5c88db90feecc6fa3b25f
Luis Güette <guetteluis@gmail.com>
2020-02-18 18:39:38 -0400

    linkit finished

--- LinkIt/AppDelegate.swift
+++ LinkIt/AppDelegate.swift
@@ -11,16 +11,54 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var item : NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+
+        item = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        item?.button?.image = NSImage(named: "link")
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Link It", action: #selector(AppDelegate.linkIt), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+        
+        item?.menu = menu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func linkIt() {
+        if let items = NSPasteboard.general.pasteboardItems {
+            for item in items {
+                for type in item.types {
+                    if type == NSPasteboard.PasteboardType(rawValue: "public.utf8-plain-text") {
+                        if let urlString = item.string(forType: type) {
+                            NSPasteboard.general.clearContents()
+                            
+                            var actualUrl = ""
+                            
+                            if urlString.hasPrefix("http://") || urlString.hasPrefix("https://") {
+                                actualUrl = urlString
+                            } else {
+                                actualUrl = "http://\(urlString)"
+                            }
+                            
+                            NSPasteboard.general.setString("<a href=\"\(actualUrl)\">\(actualUrl)</a>", forType: NSPasteboard.PasteboardType(rawValue: "public.html"))
+                            
+                            NSPasteboard.general.setString(actualUrl, forType: NSPasteboard.PasteboardType(rawValue: "public.utf8-plain-text"))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @objc func quit() {
+        NSApplication.shared.terminate(self)
+    }
 }
 
